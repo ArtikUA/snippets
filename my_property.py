e0fd6b70d63fc751e804e079492af1a11bd1401d
Artik <artik.ua@gmail.com>
2015-05-12 14:47:50 +0300

    ok

--- my_property.py
+++ my_property.py
@@ -1,1 +1,38 @@
-__author__ = 'Artik'
+class People:
+    def __init__(self, first_name, last_name):
+        self.__first_name = first_name
+        self.__last_name = last_name
+
+    @property
+    def full_name(self):
+        return " с фамилией ".join([self.__first_name, self.__last_name])
+
+    @full_name.setter
+    def full_name(self, name):
+        self.__first_name, self.__last_name = name.split() # name.split(None, 2)
+
+    @full_name.deleter
+    def full_name(self):
+        del self.__first_name
+        del self.__last_name
+
+    def __str__(self):
+        return self.full_name
+
+
+person = People("Andrey", "Popp")
+print(person.full_name)
+print()
+# Andrey с фамилией Popp
+
+person.full_name = "Vasia Pupkin"
+
+print(person.full_name)
+# Vasia с фамилией Pupkin
+
+print(person)
+# Vasia с фамилией Pupkin
+
+del person.full_name
+#print(person)
+# AttributeError: 'People' object has no attribute '_People__first_name'
--- my_string.py
+++ my_string.py
@@ -1,10 +1,47 @@
 string = 'Hi there'
 print('Found!!') if 'Hi' in string else print('Not found!!')
+# Found!!
 
+print()
 #####################
 
 cars = ['Audi', 'BMW', 'Tesla']
 
-print("My favorite car are {}" . format(', '.join(cars)))
+print("My favorite car are {}".format(', '.join(cars)))
+# My favorite car are Audi, BMW, Tesla
 
+print()
 #####################
+
+query = "login=Artik&password=123456&save=true"
+print([pair.split('=') for pair in query.split('&')])
+# [['login', 'Artik'], ['password', '123456'], ['save', 'true']]
+
+print(dict(pair.split('=') for pair in query.split('&')))
+# {'save': 'true', 'login': 'Artik', 'password': '123456'}
+
+print({pair.split('=')[0]: pair.split('=')[1] for pair in query.split('&')})
+# {'save': 'true', 'login': 'Artik', 'password': '123456'}
+
+print()
+#####################
+
+string = "my favorite game"
+
+print(string[:2])
+# my
+
+print(string[2:])
+#  favorite game
+
+print(string[3:11])
+# favorite
+
+print(string[:-2])
+# my favorite ga
+
+print(string[-2:])
+# me
+
+
+
