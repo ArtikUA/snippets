e48f759dece18105a5807cefa3b2a9190b67f5a1
ArtikUA <artik.ua@gmail.com>
2015-05-18 14:31:11 +0300

    ok

--- my_files.py
+++ my_files.py
@@ -3,15 +3,11 @@ f2 = open('2.jpg', 'wb')
 f3 = open('1.jpg', 'rb')
 f2.write(f3.read())
 
-print('aaaaa')
 
 f4 = open('1.txt', 'a')
 f4.write('aaaaa\n')
-
-print(1)
 
 f5 = open('1.txt', 'r')
 for line in f5:
     print(line + ' END')
 
-print('ololo')
