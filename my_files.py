d4463020478f8a2dd9349c65e8fd2fe1db19faf9
ArtikUA <artik.ua@gmail.com>
2015-05-18 14:20:43 +0300

    ok

--- my_files.py
+++ my_files.py
@@ -1,2 +1,11 @@
-__author__ = 'artik'
+#read 1.jpg and put in 2.jpg
+f2 = open('2.jpg', 'wb')
+f3 = open('1.jpg', 'rb')
+f2.write(f3.read())
 
+f4 = open('1.txt', 'a')
+f4.write('aaaaa\n')
+
+f5 = open('1.txt', 'r')
+for line in f5:
+    print(line + ' END')
